322fef10234d6dfbe36e02e2591317859738c502
Andrew Miller <amiller@Macintosh-c42c0311f9a0-854.local>
2015-09-20 21:38:00 -0700

    Add bmp180 board

--- arduino/PrometheusSensor.ino
+++ arduino/PrometheusSensor.ino
@@ -1,3 +1,5 @@
+
+
 /* DHTServer - ESP8266 Webserver with a DHT sensor as an input
 
    Based on ESP8266Webserver, DHTexample, and BlinkWithoutDelay (thank you)
@@ -7,8 +9,12 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <DHT.h>
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
 #define DHTTYPE DHT22
-#define DHTPIN  2
+#define DHTPIN  4
+#define DHTPWR  13
+
  
 #define WIFI_SSID           "Embedded Pie"
 #define WIFI_PASSWORD       "embedded"
@@ -33,6 +39,8 @@ ADC_MODE(ADC_VCC);
 // This is for the ESP8266 processor on ESP-01 
 DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
 
+Adafruit_BMP085 bmp;
+
 // Generally, you should use "unsigned long" for variables that hold time
 unsigned long previousMillis = 0;        // will store last temp was read
 const long interval = 2000;              // interval at which to read sensor
@@ -42,17 +50,24 @@ void setup(void)
 {
   // You can open the Arduino IDE Serial Monitor window to see what the code is doing
   Serial.begin(115200);  // Serial connection from ESP-01 via 3.3v console cable
+  // Power on the DHT22 sensor first.
+  pinMode(DHTPWR, OUTPUT);
+  digitalWrite(DHTPWR, HIGH);
   dht.begin();           // initialize temperature sensor
-
+  
   // Connect to WiFi network
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("\n\r \n\rWorking to connect");
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(250);
     Serial.print(".");
   }
   Serial.println("");
+  Wire.pins(12, 14);
+  if (!bmp.begin()) {
+    Serial.println("BMP init error!");
+  }
   Serial.println("DHT Weather Reading Client");
   Serial.print("Connected to ");
   Serial.println(WIFI_SSID);
@@ -88,21 +103,61 @@ void sendMetrics(const String& metricString) {
   client.stop();
 }
 
+bool isValidHumidity(float humidity) {
+  return (!isnan(humidity) && humidity >= 0 && humidity <= 100);
+}
+
+bool isValidTemp(float temp) {
+  return (!isnan(temp) && temp >= -100 && temp <= 212);
+}
+
+float pascalsToInchesHg(int64 pascals) {
+  return pascals * 0.000295299830714;
+}
+
+float tempCtoTempF(float tempC) {
+  return (tempC + 32) * 9 / 5;
+}
+
 void loop(void)
 {
-  float temp = dht.readTemperature(true);  // Request temperature in F
-  float humidity = dht.readHumidity();
+  float temp, humidity;
+  for (int i = 0; i < 4; i++) {
+    temp = dht.readTemperature(true);
+    if (!isValidTemp(temp)) {
+      delay(250);
+      continue;
+    }
+    break;
+  }
+  for (int i = 0; i < 4; i++) {
+    humidity = dht.readHumidity();
+    if (!isValidHumidity(humidity)) {
+      delay(250);
+      continue;
+    }
+    break;
+  }
+  
   String out;
-  if (!isnan(humidity) && humidity >= 0 && humidity <= 100) {
+  if (isValidHumidity(humidity)) {
     out += makeSensorVar("humidity") + " " + String(humidity) + "\n";
   }
-  if (!isnan(temp) && temp >= -100 && temp <= 212) {
+  if (isValidTemp(temp)) {
     out +=  makeSensorVar("tempF") + " " + String(temp) + "\n";
   }
   out += makeSensorVar("battery_millivolts") + " " + String(ESP.getVcc()) + "\n";
   out += makeSensorVar("free_heap") + " " + String(ESP.getFreeHeap()) + "\n";
+  out += makeSensorVar("bmp_tempC") + " " + String(bmp.readTemperature()) + "\n";
+  out += makeSensorVar("pressure") + " " + String(bmp.readPressure()) + "\n";
+  out += makeSensorVar("altitude") + " " + String(bmp.readAltitude()) + "\n";
   Serial.println(out);
   sendMetrics(out);
-  delay(30000);
+  // Power off the sensor while the esp is in deep sleep.
+  digitalWrite(DHTPWR, LOW);
+  ESP.deepSleep(120000000, WAKE_RF_DEFAULT);
+  // It can take a while for the ESP to actually go to sleep.
+  // When it wakes up we start again in setup().
+  delay(5000);
 } 
 
