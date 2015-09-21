c836bded2ea113a21316194a704c46afb9e95b1c
Andy Miller <z2amiller@gmail.com>
2015-09-20 22:05:43 -0700

    Clean up comments and formatting.

--- PrometheusSensor/code/PrometheusSensor.ino
+++ PrometheusSensor/code/PrometheusSensor.ino
@@ -1,11 +1,11 @@
-
-
-/* DHTServer - ESP8266 Webserver with a DHT sensor as an input
-
-   Based on ESP8266Webserver, DHTexample, and BlinkWithoutDelay (thank you)
-
-   Version 1.0  5/3/2014  Version 1.0   Mike Barela for Adafruit Industries
-*/
+/*
+ * ESP8266 sensor module.
+ * Polls a DHT22 and BMP180 sensor module and returns the results
+ * to a prometheus PushGateway.
+ * Goes in to deep sleep for 2 minutes between polling intervals, then
+ * resets and starts in setup() again.  While asleep this should draw
+ * O(100ua) and 4xAA batteries should last 4+ months.
+ */
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <DHT.h>
@@ -15,7 +15,6 @@
 #define DHTPIN  4
 #define DHTPWR  13
 
- 
 #define WIFI_SSID           "Embedded Pie"
 #define WIFI_PASSWORD       "embedded"
 
@@ -25,35 +24,34 @@
 #define METRICS_JOB         "env_sensor"
 #define METRICS_INSTANCE    "LivingRoom"
 
-const String metrics_url = "/metrics/job/" + String(METRICS_JOB) + "/instance/" + String(METRICS_INSTANCE);
+const String metrics_url = "/metrics/job/" +
+                           String(METRICS_JOB) +
+                           "/instance/" +
+                           String(METRICS_INSTANCE);
 
 ADC_MODE(ADC_VCC);
  
 // Initialize DHT sensor 
-// NOTE: For working with a faster than ATmega328p 16 MHz Arduino chip, like an ESP8266,
-// you need to increase the threshold for cycle counts considered a 1 or 0.
-// You can do this by passing a 3rd parameter for this threshold.  It's a bit
-// of fiddling to find the right value, but in general the faster the CPU the
-// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
-// Arduino Due that runs at 84mhz a value of 30 works.
-// This is for the ESP8266 processor on ESP-01 
 DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
 
+// Use the old BMP085 library - the newer ones requires the Unified Sensor
+// library which does not yet support ESP8266.
 Adafruit_BMP085 bmp;
 
-// Generally, you should use "unsigned long" for variables that hold time
 unsigned long previousMillis = 0;        // will store last temp was read
 const long interval = 2000;              // interval at which to read sensor
 
-
+// Note that with the ESP8266, waking up from deep sleep is actually done
+// with a reset, so the code will start over in setup() after every loop.
 void setup(void)
 {
-  // You can open the Arduino IDE Serial Monitor window to see what the code is doing
   Serial.begin(115200);  // Serial connection from ESP-01 via 3.3v console cable
+
   // Power on the DHT22 sensor first.
+  // This also supplies power to the bmp180 board.
   pinMode(DHTPWR, OUTPUT);
   digitalWrite(DHTPWR, HIGH);
-  dht.begin();           // initialize temperature sensor
+  dht.begin();
   
   // Connect to WiFi network
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -64,21 +62,29 @@ void setup(void)
     Serial.print(".");
   }
   Serial.println("");
-  Wire.pins(12, 14);
+
+  Wire.pins(12,  // SDA
+            14)  // SCL
   if (!bmp.begin()) {
     Serial.println("BMP init error!");
+    ESP.deepSleep(120000000, WAKE_RF_DEFAULT);
+    // It can take a while for the ESP to actually go to sleep.
+    // When it wakes up we start again in setup().
+    delay(5000);
   }
-  Serial.println("DHT Weather Reading Client");
+  Serial.println("ESP8266 Sensor Board.");
   Serial.print("Connected to ");
   Serial.println(WIFI_SSID);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
 }
 
+// Format the name of a sensor for prometheus.
 String makeSensorVar(const String& name) {
   return "# TYPE env_sensor_" + name + " gauge\n" +
-         "env_sensor_" + name + "{chipid=\"" + String(ESP.getChipId()) + "\",location=\"" + METRICS_INSTANCE + "\"}";
+         "env_sensor_" + name +
+         "{chipid=\"" + String(ESP.getChipId()) +
+         "\",location=\"" + METRICS_INSTANCE + "\"}";
 }
 
 void sendMetrics(const String& metricString) {
@@ -103,25 +109,26 @@ void sendMetrics(const String& metricString) {
   client.stop();
 }
 
-bool isValidHumidity(float humidity) {
+bool isValidHumidity(const float humidity) {
   return (!isnan(humidity) && humidity >= 0 && humidity <= 100);
 }
 
-bool isValidTemp(float temp) {
+bool isValidTemp(const float temp) {
   return (!isnan(temp) && temp >= -100 && temp <= 212);
 }
 
-float pascalsToInchesHg(int64 pascals) {
+float pascalsToInchesHg(const int64 pascals) {
   return pascals * 0.000295299830714;
 }
 
-float tempCtoTempF(float tempC) {
+float tempCtoTempF(const float tempC) {
   return (tempC + 32) * 9 / 5;
 }
 
 void loop(void)
 {
   float temp, humidity;
+  // DHT can be slow to return valid temperature and humidity.
   for (int i = 0; i < 4; i++) {
     temp = dht.readTemperature(true);
     if (!isValidTemp(temp)) {
@@ -139,6 +146,7 @@ void loop(void)
     break;
   }
   
+  // TODO(z2amiller): Use strcat or something more polite to build "out".
   String out;
   if (isValidHumidity(humidity)) {
     out += makeSensorVar("humidity") + " " + String(humidity) + "\n";
@@ -146,8 +154,10 @@ void loop(void)
   if (isValidTemp(temp)) {
     out +=  makeSensorVar("tempF") + " " + String(temp) + "\n";
   }
+  // TODO(z2amiller): Poll the actual battery voltage not the LDO output.
   out += makeSensorVar("battery_millivolts") + " " + String(ESP.getVcc()) + "\n";
   out += makeSensorVar("free_heap") + " " + String(ESP.getFreeHeap()) + "\n";
+  // TODO(z2amiller): Sanitize the bmp180 outputs.
   out += makeSensorVar("bmp_tempC") + " " + String(bmp.readTemperature()) + "\n";
   out += makeSensorVar("pressure") + " " + String(bmp.readPressure()) + "\n";
   out += makeSensorVar("altitude") + " " + String(bmp.readAltitude()) + "\n";
